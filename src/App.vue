26540c95c7a83b57c721a338c743ff4ee8565433
DanieleSalinitro <daniele.salinitro@icloud.com>
2024-06-13 11:54:52 +0200

    modifiche

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <HeaderComponent></HeaderComponent>
     <h1>Progetti</h1>
     <ul>
       <li v-for="project in projects" :key="project.id">
@@ -11,8 +12,14 @@
 
 <script>
 import axios from 'axios';
+import HeaderComponent from './components/HeaderComponent.vue';
+
 
 export default {
+  name: 'App',
+  components: {
+    HeaderComponent
+  },
   data() {
     return {
       projects: []
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,12 +1,41 @@
 <template>
     <div>
-      <h1>Progetti</h1>
-      <p>Questi sono i progetti:</p>
+      <h1>Projects</h1>
+      <div class="row">
+        <div class="col-md-4" v-for="project in projects" :key="project.id">
+          <CardComponent :project="project" />
+        </div>
+      </div>
     </div>
   </template>
   
   <script>
+  import axios from 'axios';
+  import CardComponent from '../components/CardComponent.vue';
+  
   export default {
-    name: 'Projects'
+    name: 'Projects',
+    components: {
+      CardComponent
+    },
+    data() {
+      return {
+        projects: []
+      };
+    },
+    created() {
+      axios.get('http://localhost:8000/api/projects')
+        .then(response => {
+          this.projects = response.data;
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching projects:', error);
+        });
+    }
   };
   </script>
+  
+  <style scoped>
+  /* Aggiungi eventuali stili personalizzati qui */
+  </style>
